2db438b8482d990f732ad5f8f7336da11789ddd4
NilsonKr <nuotrok444@gmail.com>
2022-04-07 21:17:07 -0500

    :sparkles: Adding ERC165 support interface in ERC20 contract

--- contracts/tokens/ERC20Token.sol
+++ contracts/tokens/ERC20Token.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
-contract ERC20Token is ERC20 {
+contract ERC20Token is ERC20, ERC165 {
     constructor(uint256 initialSupply) ERC20("Papu Token", "PAP") {
         _mint(msg.sender, initialSupply);
     }
+
+    function supportsInterface(bytes4 interfaceId) public view override returns(bool){
+        return interfaceId == type(IERC20).interfaceId || super.supportsInterface(interfaceId);
+    } 
 }
